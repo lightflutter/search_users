33bb06a75ef972972b6504719eb8a1fa99bb7435
lightflutter <iamwaizord@gmail.com>
2023-10-03 12:41:33 +0300

    feat: make model with errorResponse

--- lib/Data/Model/user_response.dart
+++ lib/Data/Model/user_response.dart
@@ -1,6 +1,16 @@
 import 'package:search_users/Data/Model/user.dart';
-class UserResponse {
-  final User user;
 
-  UserResponse(this.user);
+class UserResponse {
+  final User results;
+  final String error;
+
+  UserResponse(this.results, this.error);
+
+  UserResponse.fromJson(Map<String, dynamic> json)
+      : results = User.fromJson(json),
+        error = '';
+
+  UserResponse.withError(String errorValue)
+      : results = User.empty(),
+        error = errorValue;
 }
