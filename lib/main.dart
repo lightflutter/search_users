ec30ddb8603d47ccaaf8e66ef615d85f7ec3624a
lightflutter <iamwaizord@gmail.com>
2023-10-11 23:58:47 +0300

    feat: make into main load data

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,19 @@
-import 'package:flutter/material.dart';
+import 'package:search_users/Data/Model/user_response.dart';
+import 'package:search_users/Data/user_repository.dart';
+import 'package:search_users/app.dart';
+import 'package:bloc/bloc.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:search_users/counter_observer.dart';
 
-void main() {
-  runApp(const MyApp());
-}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+void main() async {
+  final UserRepository userRepository = UserRepository();
+  final UserResponse userResponse = await userRepository.getUser('waizord');
+  if(userResponse.error.isNotEmpty) {
+    print('ERROR ${userResponse.error}');
+  } else {
+    print(userResponse.results.name);
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
+  Bloc.observer = const CounterObserver();
+  runApp(const CounterApp());
 }
